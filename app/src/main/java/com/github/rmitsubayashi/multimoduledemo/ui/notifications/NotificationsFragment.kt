ffeb377827ea65b6c25cb18e54a2baed000a619f
rmitsubayashi <ryomitsu@gmail.com>
2019-10-09 21:22:21 +0900

    remove unused dependencies

--- app/src/main/java/com/github/rmitsubayashi/multimoduledemo/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/github/rmitsubayashi/multimoduledemo/ui/notifications/NotificationsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.github.rmitsubayashi.multimoduledemo.R
 import org.koin.android.viewmodel.ext.android.viewModel
 
